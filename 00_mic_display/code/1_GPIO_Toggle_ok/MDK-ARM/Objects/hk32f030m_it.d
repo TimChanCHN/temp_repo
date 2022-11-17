1364ed2697ad781901997f49297599ac1128c77e
TimChanCHN <874797636@qq.com>
2022-11-17 23:31:48 +0800

    delete build files

--- 00_mic_display/code/1_GPIO_Toggle_ok/MDK-ARM/Objects/hk32f030m_it.d
+++
@@ -1,2 +0,0 @@
-.\objects\hk32f030m_it.o: ..\src\hk32f030m_it.c
-.\objects\hk32f030m_it.o: ..\inc\hk32f030m_it.h
--- 00_mic_display/code/1_GPIO_Toggle_ok/MDK-ARM/Objects/keil_startup_hk32f030m.d
+++
@@ -1,1 +0,0 @@
-.\objects\keil_startup_hk32f030m.o: ..\..\HK32F030M\CMSIS\HK32F030M\Source\ARM\KEIL_Startup_hk32f030m.s
